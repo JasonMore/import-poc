ec97a7e6fbd973f26cb7bdfdf18888b96c08dd25
Jason More <Jason.More@gmail.com>
2014-05-13 08:24:31 -0500

    fix build

--- server/service/derpService.spec.js
+++ server/service/derpService.spec.js
@@ -1,8 +1,7 @@
 describe('derpService >', function () {
-  var expect = require('expect.js'),
    derpService = require('./derpService');
 
   it('adds', function () {
-    expect(derpService.derp('hello', 'world')).to.equal('hello:world');
+    expect(derpService.derp('hello', 'world')).toEqual('hello:world');
   });
 });
